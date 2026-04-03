53d37efaf45de79e92c87f5db91224e314a762cc
abu-zakariya <mahmoudaa95@gmail.com>
2026-04-03 18:54:09 +0200

    wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://islamischeliteratur.de",
   output: "static",
+
   vite: {
     cacheDir: ".cache/vite",
     server: {
       host: true
     }
-  }
+  },
+
+  adapter: cloudflare()
 });
